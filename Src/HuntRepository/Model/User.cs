f35de769f49227381d1e724d7d1d6618c652ea01
patrykapriasz <patrykapriasz@gmail.com>
2019-02-13 18:34:20 +0100

    dodanie adnotacji do modelu user, role

--- Src/HuntRepository/Model/User.cs
+++ Src/HuntRepository/Model/User.cs
@@ -8,10 +8,17 @@ namespace Hunt.Model
         [Key]
         public Guid Identifier { get; set; }
         public DateTime Issued { get; set; }
+        [Required]
         public string Login { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Surname { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [MinLength(8)]
         public string Password { get; set; }
         public Role Role { get; set; }
     }
