1ddeae5eddac8d2e47ebf1dac98048371a59ba9b
patrykapriasz <patrykapriasz@gmail.com>
2019-02-14 18:30:48 +0100

    uaktualnienie modelu

--- Src/HuntRepository/Data/HuntContext.cs
+++ Src/HuntRepository/Data/HuntContext.cs
@@ -5,8 +5,6 @@ namespace Hunt.Data
 {
     public class HuntContext : DbContext{
 
-        private string password = "";
-        private string user = "";
         public HuntContext(DbContextOptions<HuntContext> options):base(options) 
         {
             
@@ -14,8 +12,7 @@ namespace Hunt.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            var connectionString=$"Data Source=mssql6.webio.pl,2401;Initial Catalog=patrykapriasz_jaegerPrime;User ID={user};Password={password}";
-            optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=hunting;Trusted_Connection=True;MultipleActiveResultSets=true");            
+            
         }  
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
